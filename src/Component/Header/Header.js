fc7b930e4be1d2d5193102349e0533ada0c06f90
raselalamjubayed <raselalam838@gmil.com>
2021-09-26 21:51:29 +0600

    site added

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
     return (
        <div className="color container mb-4">
-           <h1>International Millioner of 2021</h1>
+           <h1>International <span style={{color: 'goldenrod'}}>Millioner</span> of 2021</h1>
            <h3>Budget of 100 Million Dollar</h3>
        </div> 
     );
